1294887a679db72a94093f4e2265b2c15fc7ca91
3tresearchdevelopment <3tresearchdevelopment@gmail.com>
2016-05-24 22:24:59 -0400

    first draft

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav({
+    	edge: 'left',
+    	closeOnClick: true
+    });
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
