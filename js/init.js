fcbe595d6e58836e2b453112de59f88404a8eaff
3tresearchdevelopment <3tresearchdevelopment@gmail.com>
2016-06-02 01:00:06 -0400

    changed layout to one page

--- js/init.js
+++ js/init.js
@@ -5,6 +5,8 @@
     	edge: 'left',
     	closeOnClick: true
     });
-
+    $(".rslides").responsiveSlides({
+        timeout: 2000
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
